abb5a0dbb08deaaa5181a8693d56e31e3745e354
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2022-12-11 17:00:02 +0100

    redesign loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,50 @@
 import styles from "./Loader.module.css";
 
+const Shimmer = () => {
+  return (
+    <div className={styles.loader_wrapper}>
+      <div className={styles.left_loader_product_wrapper}>
+        <div className={styles.product_loader_thumbnail_section}>
+          <div className={styles.loader_thumbnail}></div>
+
+          <div className={styles.loader_thumbnail_info}>
+            <div className={styles.loader_label}></div>
+            <p className={styles.loader_thumbnail_title}></p>
+            <button></button>
+          </div>
+        </div>
+
+        <table className={styles.loading_product_info}>
+          <tbody>
+            <tr>
+              <td>Rating</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Discount Percentage</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Stock</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <p className={styles.loading_product_description}></p>
+      </div>
+
+      <div className={styles.loader_image_wrapper}></div>
+    </div>
+  );
+};
+
 const Loader = () => {
   return (
-    <div className={styles.loader}>
-      <h1>Loading...</h1>
-    </div>
+    <>
+      <Shimmer />
+      <Shimmer />
+    </>
   );
 };
 
